1c529eef52319e0a7bc5e256b4bba2a374b21a02
ALUMNEDAM <mcerezo@ieslaferreria.net>
2016-11-08 16:04:08 +0100

    Vista i control començat

---
+++ src/domino/vista/VistaText.java
@@ -0,0 +1,19 @@
+package domino.vista;
+
+/**
+ *
+ * @author Matias
+ */
+public class VistaText {
+
+    public void imprimirDadesTorn(int torn, String nom) {
+        System.out.println("TORN: " +torn + "\t" + nom);
+    }
+            
+    public void mostrarMenuJoc() {
+        System.out.println("TRIAR JUGADA:\n" +
+                "1. Col·locar fitxa.\n" +
+                "2. Co·locar dues fitxes dobles.\n"+
+                "3. Passar torn.\n");       
+    }
+}
